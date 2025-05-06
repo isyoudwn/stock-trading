9a00411787b79bee4ce54293758e56c518ea2045
임채연 <dlacodus0407@gmail.com>
2025-05-07 08:23:27 +0900

    meta data 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'stock-trading',
+  description: 'stock-trading',
   generator: 'v0.dev',
 }
 
